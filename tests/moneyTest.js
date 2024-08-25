9ccc6e920d8e71ce3ee8ba08e5ba0e14bc08ff6b
Favadh <favadh10@gmail.com>
2024-08-25 09:44:34 +0530

    Remove defautlt tests and add money tests in jasmine

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -12,6 +12,10 @@ import formatCurrency from "../scripts/utils/money.js";
   
   A group of related tests is called test suite.*/
 
+console.log('test suite: formatCurrency');
+
+console.log('converts cents into dollars');
+
 if (formatCurrency(2095) === '20.95') {
   console.log('passed');
 }
@@ -19,12 +23,16 @@ else {
   console.log('failed');
 }
 
+console.log('works with zero');
+
 if (formatCurrency(0) === '0.00') {
   console.log('passed');
 }
 else {
   console.log('failed');
 }
+
+console.log('rounding cents to the nearest nbr');
 
 if (formatCurrency(2000.5) === '20.01') {
   console.log('passed');
